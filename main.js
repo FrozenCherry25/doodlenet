d09e634b2774333eaf6b5bd7b82d684473fa2d0a
FrozenCherry25 <80614501+FrozenCherry25@users.noreply.github.com>
2022-05-09 12:47:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function setup() {
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white");
+}
+
+function preload() {
+    doodle= loadImage('https://i.postimg.cc/NfVY8tgB/3204572.jpg');
+}
